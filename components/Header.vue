93231c1e93f86e81acff97899ea5a3f2001f7d11
ifeelsoempty <brayan.1702@mail.ru>
2021-01-12 12:12:21 +0400

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header-wrapper">
+        <div class="header-logo-wrapper">
+          <div class="header-logo">
+            <img src="@/assets/images/logo-white.png" alt="logo">
+          </div>
+        </div>
+        <div class="header-menu">
+          <ul class="header-navbar">
+            <li>Проекты</li>
+            <li>Услуги</li>
+            <li>Команда</li>
+          </ul>
+          <div class="header-burger">
+            <input id="header-burger-toggle" class="header-burger-toggle" type="checkbox" />
+            <label class="header-burger-btn" for="header-burger-toggle">
+              <span></span>
+            </label>
+            <div class="header-burger-box">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="background: #2C2A2A; width: 100vw; height: 100vh">
+    <!-- Content -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
