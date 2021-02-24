837931504dc0b992b77a0a555dfde6fb423b2e07
ifeelsoempty <brayan.1702@mail.ru>
2021-02-24 16:54:23 +0400

    remove preview

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FMoscow&amp;src=b245YmV1aGtlMmhxaTFxbXQ1MTVwaTNmMnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%234285F4&amp;mode=WEEK&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-    <!-- <Header />
+    <Header />
     <Nuxt />
-    <CustomCursor /> -->
+    <CustomCursor />
   </div>
 </template>
 
