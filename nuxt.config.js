825e56202b0f916ab48598ba343b4594eb5b925c
ifeelsoempty <brayan.1702@mail.ru>
2021-02-25 14:38:30 +0400

    add robots.txt

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,13 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    '@nuxtjs/robots',
   ],
+
+  robots: {
+    UserAgent: '*',
+    Disallow: '/'
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
